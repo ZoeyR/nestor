ef46292aabfe1eee0d70babd6447e6367ca48193
Daniel Griffen <daniel@griffen.io>
2019-03-19 18:51:15 -0700

    add forward tests

--- nestor/tests/end_to_end.rs
+++ nestor/tests/end_to_end.rs
@@ -1,12 +1,17 @@
 #![feature(await_macro, async_await, futures_api)]
 
-use std::thread;
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufRead, Read, Write};
+use std::thread;
 
 use nestor::command;
 use nestor::response::{Outcome, Response};
 use nestor::Nestor;
+
+#[command("die")]
+fn die() {
+    panic!()
+}
 
 #[command("speak")]
 fn speak() -> Outcome {
@@ -23,39 +28,105 @@ fn note() -> Outcome {
     Outcome::Success(Response::Notice("notice".into()))
 }
 
+#[command("forward")]
+fn forward() -> Outcome {
+    Outcome::Forward("forward-to".into())
+}
+
+#[command("forward-to")]
+fn forward_to() -> Outcome {
+    Outcome::Success(Response::Say("forwarded".into()))
+}
+
+#[command("forward-loop")]
+fn forward_loop() -> Outcome {
+    Outcome::Forward("forward-loop".into())
+}
+
 #[test]
 fn bot_say() {
     let mut stream = setup(44444);
-    stream.write_all(b":Testhost PRIVMSG test :~speak\r\n").unwrap();
+    stream
+        .write_all(b":Testhost PRIVMSG test :~speak\r\n")
+        .unwrap();
 
-    let reader = BufReader::new(stream);
+    let reader = BufReader::new(&stream);
     let line = reader.lines().skip(3).next().unwrap().unwrap();
 
     assert_eq!(line, "PRIVMSG Testhost :hello");
+
+    stream
+        .write_all(b":Testhost PRIVMSG test :~die\r\n")
+        .unwrap();
 }
 
 #[test]
 fn bot_act() {
     let mut stream = setup(44445);
-    stream.write_all(b":Testhost PRIVMSG test :~act\r\n").unwrap();
+    stream
+        .write_all(b":Testhost PRIVMSG test :~act\r\n")
+        .unwrap();
 
-    let reader = BufReader::new(stream);
+    let reader = BufReader::new(&stream);
     let line = reader.lines().skip(3).next().unwrap().unwrap();
 
     assert_eq!(line, "PRIVMSG Testhost :\u{1}ACTION action\u{1}");
+
+    stream
+        .write_all(b":Testhost PRIVMSG test :~die\r\n")
+        .unwrap();
 }
 
 #[test]
 fn bot_notice() {
     let mut stream = setup(44446);
-    stream.write_all(b":Testhost PRIVMSG test :~note\r\n").unwrap();
+    stream
+        .write_all(b":Testhost PRIVMSG test :~note\r\n")
+        .unwrap();
 
-    let reader = BufReader::new(stream);
+    let reader = BufReader::new(&stream);
     let line = reader.lines().skip(3).next().unwrap().unwrap();
 
     assert_eq!(line, "NOTICE Testhost :notice");
+
+    stream
+        .write_all(b":Testhost PRIVMSG test :~die\r\n")
+        .unwrap();
 }
 
+#[test]
+fn bot_forward() {
+    let mut stream = setup(44447);
+    stream
+        .write_all(b":Testhost PRIVMSG test :~forward\r\n")
+        .unwrap();
+
+    let reader = BufReader::new(&stream);
+    let line = reader.lines().skip(3).next().unwrap().unwrap();
+
+    assert_eq!(line, "PRIVMSG Testhost :forwarded");
+
+    stream
+        .write_all(b":Testhost PRIVMSG test :~die\r\n")
+        .unwrap();
+}
+
+#[test]
+fn bot_forward_loop() {
+    let mut stream = setup(44449);
+    stream
+        .write_all(b":Testhost PRIVMSG test :~forward-loop\r\n")
+        .unwrap();
+
+    let reader = BufReader::new(&stream);
+    let line = reader.lines().skip(3).next().unwrap().unwrap();
+
+    assert_eq!(line, "NOTICE Testhost :alias depth too deep");
+
+    stream
+        .write_all(b":Testhost PRIVMSG test :~die\r\n")
+        .unwrap();
+}
 
 fn setup(port: u16) -> TcpStream {
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
@@ -73,10 +144,10 @@ fn setup(port: u16) -> TcpStream {
             port,
         );
         let config = toml::de::from_str(&config_str).unwrap();
-
-        Nestor::with_config(config).activate();
-        
+        let _ = std::panic::catch_unwind(|| {
+            Nestor::with_config(config).activate();
+        });
     });
-    
+
     listener.incoming().next().unwrap().unwrap()
 }
